5ba73d55d6731c5e9842c1cb59073b99086e553c
ari amin <109306820+randyansyah@users.noreply.github.com>
2024-01-31 18:54:05 +0700

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -15,19 +15,19 @@ if [ -f /etc/redhat-release ]; then
     yum install -y docker-ce docker-ce-cli containerd.io
 elif [ -f /etc/debian_version ]; then
     # Debian / Ubuntu
-    apt-get update
-    apt-get install -y \
+    sudo apt-get update
+    sudo apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
         gnupg \
         lsb-release
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo echo \
         "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
         $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 else
     echo "Distribusi tidak didukung"
     exit 1
